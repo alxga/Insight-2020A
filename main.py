980572600d1bd19d35cc20791a1f438ddef9385d
Alexander Ganin <alexander_a_g@outlook.com>
2020-01-15 15:22:32 -0500

    Based threading, 5 second resolution

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import os
+import time
 from datetime import datetime
 import requests
+import threading
 
-url = "https://cdn.mbta.com/realtime/VehiclePositions.pb"
-url = "http://www.pdf995.com/samples/pdf.pdf"
-r = requests.get(url)
-print("Retrieved %d bytes\n" % len(r.content))
+Feeds = [
+    ("VehiclePos", "https://cdn.mbta.com/realtime/VehiclePositions.pb"),
+    ("TripUpdates", "https://cdn.mbta.com/realtime/TripUpdates.pb"),
+    ("Alerts", "https://cdn.mbta.com/realtime/Alerts.pb")
+]
 
-DumpFName = datetime.now().strftime("%Y%m%d-%H%M%S.pb")
 PyPath = os.path.dirname(os.path.abspath(__file__))
-DumpDir = os.path.join(PyPath, "VP")
-if not os.path.exists(DumpDir):
-    os.makedirs(DumpDir)
-DumpFilePath = os.path.join(DumpDir, DumpFName)
+for tpl in Feeds:
+    p = os.path.join(PyPath, tpl[0])
+    if not os.path.exists(p):
+        os.makedirs(p)
 
-with open(DumpFilePath, "wb") as handle:
-    handle.write(r.content)
+
+def download_feed(dir, url):
+    fName = datetime.now().strftime("%Y%m%d-%H%M%S.pb")
+    r = requests.get(url)
+    fPath = os.path.join(PyPath, dir, fName)
+    with open(fPath, "wb") as handle:
+        handle.write(r.content)
+
+
+threads = []
+for sec in range(0, 59, 5):
+    print("Offset %d\n" % sec)
+    for tpl in Feeds:
+        t = threading.Thread(target=download_feed, args=tpl)
+        t.start()
+        threads.append(t)
+    time.sleep(5)
+
+for t in threads:
+    t.join()
