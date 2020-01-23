854e2aaffc6c995dcb97bf7f954a25d320ee7bc5
Alexander Ganin <alexander_a_g@outlook.com>
2020-01-23 18:59:09 -0500

    Added basic spark tester script

---
+++ spk_pbparser1.py
@@ -0,0 +1,37 @@
+# pylint: disable=unused-import
+from operator import add
+
+import pyspark
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+import boto3
+
+_s3Bucket = "alxga-insde"
+_s3Res = boto3.resource('s3')
+
+
+def fetch_keys():
+  objKeys = []
+  bucket = _s3Res.Bucket(_s3Bucket)
+  for obj in bucket.objects.filter(Prefix='pb/VehiclePos'):
+    objKeys.append(obj.key)
+    if len(objKeys) > 100:
+      break
+  return objKeys
+
+if __name__ == "__main__":
+  spark = SparkSession\
+      .builder\
+      .appName("PythonTestScript")\
+      .getOrCreate()
+
+  keys = fetch_keys()
+  lines = lines.rdd.map(lambda r: r[0])
+  counts = lines.flatMap(lambda x: x.split(' ')) \
+                .map(lambda x: (x, 1)) \
+                .reduceByKey(add)
+  output = counts.collect()
+  for (word, count) in output:
+    print("%s: %i" % (word, count))
+
+  spark.stop()
