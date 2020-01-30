8fcece0d9860aebe722ed8d3f61521518be46fad
Alexander Ganin <alexander_a_g@outlook.com>
2020-01-30 11:25:40 -0500

    Updated VS Professional Project

--- sqlscripts/createVehPosPb.sql
+++
@@ -1,5 +0,0 @@
-create table VehPosPb(
-  S3Key char(50) Primary Key,
-  NumRecs integer,
-  SDate DateTime,
-  EDate DateTime)
--- sqlscripts/insertVehPosPb.sql
+++
@@ -1,2 +0,0 @@
-insert ignore into VehPosPb(S3Key, NumRecs, SDate, EDate)
-values (%s, %s, %s, %s)
