afb317d3c50ee70acb794ef2f792a1d3cb14faea
Alexander Ganin <alexander_a_g@outlook.com>
2020-02-11 20:35:16 -0500

    Added a redirect to Google Slides from /mbta/slides

--- frontend/mbta/routes.py
+++ frontend/mbta/routes.py
@@ -13,3 +13,9 @@ def index():
   return render_template(
     'index.html', appName=Settings.AppName, footerLine=Settings.FooterLine
   )
+
+
+@bp.route('/slides', methods=['GET'])
+def slides():
+  slides_url = "https://docs.google.com/presentation/d/1AwABJ4udwEj1ofg99GbL5I2vhLG6S0Bxh877UUzRBKE/edit?usp=sharing"
+  return redirect(slides_url, code=302)
